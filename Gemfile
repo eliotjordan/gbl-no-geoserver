3f7e1f5751384a7aa87f8ba598d1eaadcb419afb
Eliot Jordan <eliotj@princeton.edu>
2023-07-18 15:37:03 -0500

    Use vite and test openlayers

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem "sqlite3", "~> 1.4"
 gem "stimulus-rails"
 gem "turbo-rails"
 gem "twitter-typeahead-rails", "0.11.1.pre.corejavascript"
+gem "vite_rails"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ app/javascript/entrypoints/application.js
@@ -0,0 +1,43 @@
+// import '../stylesheets/openlayers.css';
+// import {Map, View} from 'ol';
+// import TileLayer from 'ol/layer/Tile';
+// import VectorTile from 'ol/layer/VectorTile';
+// import OSM from 'ol/source/OSM'
+// import {PMTilesVectorSource} from 'ol-pmtiles';
+// import { useGeographic } from 'ol/proj';
+// import {Style, Stroke, Fill} from 'ol/style';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.getElementById('ol-map')
+  const data = element.dataset
+
+  if(data.protocol == 'Pmtiles') {
+    const bbox = new ol.format.GeoJSON().readFeatures(data.mapGeom)[0].getGeometry().getExtent()
+    const baseLayer = new ol.layer.Tile({ source: new ol.source.OSM() });
+    const vectorLayer = new ol.layer.VectorTile({
+        declutter: true,
+        source: new olpmtiles.PMTilesVectorSource({
+          // url: "https://pul-tile-images.s3.amazonaws.com/Dept_of_Public_Works_Roadwork_Projects.pmtiles",
+          // url: "https://pul-tile-images.s3.amazonaws.com/pmtiles/parcels.pmtiles",
+          url: data.url
+        }),
+        style: new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: 'gray',
+            width: 1,
+          }),
+          fill: new ol.style.Fill({
+            color: 'rgba(20,20,20,0.9)',
+          })
+        })
+      });
+
+      ol.proj.useGeographic();
+      const map = new ol.Map({
+        layers: [baseLayer, vectorLayer],
+        target: 'ol-map'
+      });
+      map.getView().fit(bbox, map.getSize());
+  }
+})
