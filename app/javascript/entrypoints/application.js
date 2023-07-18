a0130095a2c9eb0ed790db08dd30635bf074470d
Eliot Jordan <eliotj@princeton.edu>
2023-07-18 17:29:45 -0500

    Work on COG

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -3,41 +3,44 @@
 // import TileLayer from 'ol/layer/Tile';
 // import VectorTile from 'ol/layer/VectorTile';
 // import OSM from 'ol/source/OSM'
-// import {PMTilesVectorSource} from 'ol-pmtiles';
+// import GeoJSON from 'ol/format/geojson'
 // import { useGeographic } from 'ol/proj';
 // import {Style, Stroke, Fill} from 'ol/style';
-import { PMTilesVectorSource } from '@/components/pmtiles-layer'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const element = document.getElementById('ol-map')
-  const data = element.dataset
-
-  if(data.protocol == 'Pmtiles') {
-    const extent = new ol.format.GeoJSON().readFeatures(data.mapGeom)[0].getGeometry().getExtent()
-    const baseLayer = new ol.layer.Tile({ source: new ol.source.OSM() });
-    const vectorLayer = new ol.layer.VectorTile({
-        declutter: true,
-        source: new PMTilesVectorSource({
-          // url: "https://pul-tile-images.s3.amazonaws.com/Dept_of_Public_Works_Roadwork_Projects.pmtiles",
-          // url: "https://pul-tile-images.s3.amazonaws.com/pmtiles/parcels.pmtiles",
-          url: data.url
-        }),
-        style: new ol.style.Style({
-          stroke: new ol.style.Stroke({
-            color: 'gray',
-            width: 1,
-          }),
-          fill: new ol.style.Fill({
-            color: 'rgba(20,20,20,0.9)',
-          })
-        })
-      });
-
-      ol.proj.useGeographic();
-      const map = new ol.Map({
-        layers: [baseLayer, vectorLayer],
-        target: 'ol-map'
-      });
-      map.getView().fit(extent, map.getSize());
-  }
-})
+// import { PMTilesVectorSource } from '@/components/pmtiles-layer'
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   const element = document.getElementById('xxmap')
+//   if (!element) return
+//   const data = element.dataset
+//   if (data.protocol == 'Pmtiles') {
+//     const geom = data.mapGeom
+//     const extent = new GeoJSON().readFeatures(geom)[0].getGeometry().getExtent()
+//     const baseLayer = new TileLayer({ source: new OSM() });
+//     const vectorLayer = new VectorTile({
+//         declutter: true,
+//         source: new PMTilesVectorSource({
+//           // url: "https://pul-tile-images.s3.amazonaws.com/Dept_of_Public_Works_Roadwork_Projects.pmtiles",
+//           // url: "https://pul-tile-images.s3.amazonaws.com/pmtiles/parcels.pmtiles",
+//           url: data.url
+//         }),
+//         style: new Style({
+//           stroke: new Stroke({
+//             color: '#7070B3',
+//             width: 1,
+//           }),
+//           fill: new Fill({
+//             color: '#FFFFFF',
+//           })
+//         })
+//       });
+//
+//       useGeographic();
+//       const map = new Map({
+//         layers: [baseLayer, vectorLayer],
+//         target: 'map'
+//       });
+//       map.getView().fit(extent, map.getSize());
+//   } else if (data.protocol == 'Cog') {
+//
+//   }
+// })
